93a8d66812ad9a4a18eb2ca37707eb23a142e552
Jaroslav Šmolík <grissius@gmail.com>
2017-11-22 19:36:07 +0100

    Update triplify.sh

--- semestral/download/triplifier/make_triples.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# tarql map.sparql ../../source/out.csv > ../output/output.ttl
-tarql map.sparql ../../source/out.csv
---
+++ semestral/download/triplifier/triplify.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+tarql map.sparql ../../source/out.csv > ../output/output.ttl
