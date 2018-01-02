e90caaba0b99e232fbaeb56b9c81cef4bf8d4ad8
Jaroslav Šmolík <grissius@gmail.com>
2018-01-02 10:32:44 +0100

    Add freq triplifier

--- semestral/download/triplifier/triplify.sh
+++ semestral/download/triplifier/triplify.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 tarql hunspell.sparql ../../source/out.hunspell.csv > ../output/out.hunspell.ttl
 tarql stopword.sparql ../../source/out.stopword.csv > ../output/out.stopword.ttl
+tarql freq.sparql ../../source/out.freq.csv > ../output/out.freq.ttl
