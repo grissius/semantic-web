cffad764f6c738b5cab0cf8452f02f1f9f2dddb7
Jaroslav Šmolík <grissius@gmail.com>
2018-01-02 11:54:26 +0100

    Add wiktionary triplifier

--- semestral/download/triplifier/triplify.sh
+++ semestral/download/triplifier/triplify.sh
@@ -2,3 +2,4 @@
 tarql hunspell.sparql ../../source/out.hunspell.csv > ../output/out.hunspell.ttl
 tarql stopword.sparql ../../source/out.stopword.csv > ../output/out.stopword.ttl
 tarql freq.sparql ../../source/out.freq.csv > ../output/out.freq.ttl
+tarql wiktionary.sparql ../../source/out.wiktionary.csv > ../output/out.wiktionary.ttl
